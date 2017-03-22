23453de071b98c35633757a3532cc94c5d1effdc
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-03-22 22:52:26 +0300

    Add Category.

---
+++ src/main/java/com/github/kamys/managerProduct/logic/Category.java
@@ -0,0 +1,8 @@
+package com.github.kamys.managerProduct.logic;
+
+/**
+ * Category can consist only other {@link Section}.
+ * Need for to create the tree of {@link Section}.
+ */
+class Category extends Section {
+}
